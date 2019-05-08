9ee0157bf62bf85b9d1282b1e049327f3a9a67bb
Rale <sapicr23@gmail.com>
2019-05-09 00:13:28 +0200

    Proba za pamcenje prekidne rutine

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,45 @@
 #include <iostream.h>
 #include <dos.h>
+#include <stdio.h>
+typedef void interrupt (*pInterrupt)(...);
 
+volatile int cntr=0;
+pInterrupt oldISR,keyboardISR;
+
+void inic()
+{
+	asm cli;
+	oldISR=getvect(9);
+	setvect(9,keyboardISR);
+	asm sti;
+}
+
+void restore()
+{
+	asm cli;
+	setvect(9,oldISR);
+	asm sti;
+}
+
+void doSomething()
+{
+	for(int i=0;i<10;i++) {
+
+		printf("main %d\n",i);
+
+		for(int j=0;j<30000;++j)
+			for(int k=0;k<30000;k++);
+	}
+
+	printf("Tastatura je generisala dogadjaj!\n",cntr);
+}
 int main(){
 
-	cout << "Hello world!";
+	inic();
+
+	doSomething();
+
+	restore();
 
 	return 0;
 }
